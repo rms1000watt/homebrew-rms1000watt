a2d5c31bcc98cb367a3a1df32907a72d534636e5
Ryan Smith <rms1000watt@gmail.com>
2018-11-29 01:38:16 -0800

    version lock

--- kubenukem.rb
+++ kubenukem.rb
@@ -1,8 +1,9 @@
 class Kubenukem < Formula
   homepage "https://gist.github.com/rms1000watt/999a25e2b57d9bc0c5b9ebc06c890174"
 
-  url "https://raw.githubusercontent.com/rms1000watt/kubenukem/master/kubenukem"
-  version "0.0.2"
+  v = "0.0.2"
+  url "https://raw.githubusercontent.com/rms1000watt/kubenukem/#{v}/kubenukem"
+  version v
   # openssl dgst -sha256 ../kubenukem/kubenukem
   sha256 "5248bc05200a926d2a14bc4429bdd5b20a88b2217079fa211989e302f229d29a"
 
