81eb20cb0b7a8efddf2bdaf83d3ca8cf6d35a92b
Ryan Smith <rms1000watt@gmail.com>
2018-11-29 01:25:03 -0800

    wip kubenukem

--- kubenukem.rb
+++ kubenukem.rb
@@ -1,15 +1,12 @@
-# require "FileUtils"
-
 class Kubenukem < Formula
   homepage "https://gist.github.com/rms1000watt/999a25e2b57d9bc0c5b9ebc06c890174"
 
   url "https://gist.githubusercontent.com/rms1000watt/999a25e2b57d9bc0c5b9ebc06c890174/raw/102cceaa197894a0e6437efdc417851b3bc554c5/kubenukem"
   version "0.0.1"
   # openssl dgst -sha256 kubenukem
-  sha256 "5c887b07a77098f815f761ac841afae9f53663f0dea1ee99428e046caae968cd"
+  sha256 "105cac51a7b0841046f0aa714f5b6cce717562b1638443978d1c41b6d135dd37"
 
   def install
-    # FileUtils.mv("hello-world-homebrew-darwin-amd64", "hello-world-homebrew")
     bin.install "kubenukem"
   end
 
