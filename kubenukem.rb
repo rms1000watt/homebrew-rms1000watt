0783e360ddccfd50375967bd60b2ad95d6569b78
Ryan Smith <rms1000watt@gmail.com>
2018-11-29 01:22:14 -0800

    wip kubenukem

--- kubenukem.rb
+++ kubenukem.rb
@@ -4,6 +4,7 @@ class Kubenukem < Formula
   homepage "https://gist.github.com/rms1000watt/999a25e2b57d9bc0c5b9ebc06c890174"
 
   url "https://gist.githubusercontent.com/rms1000watt/999a25e2b57d9bc0c5b9ebc06c890174/raw/102cceaa197894a0e6437efdc417851b3bc554c5/kubenukem"
+  version "0.0.1"
   # openssl dgst -sha256 kubenukem
   sha256 "5c887b07a77098f815f761ac841afae9f53663f0dea1ee99428e046caae968cd"
 
